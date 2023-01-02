2a071eb19cd0a6d0a6b0e80bedf5c4d44fd8555d
chechen-respublic <121053834+chechen-respublic@users.noreply.github.com>
2023-01-02 07:19:22 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-let socket = new WebSocket("ws://http://56c0-188-0-188-195.ngrok.io");
+let socket = new WebSocket("ws://56c0-188-0-188-195.ngrok.io");
 
 socket.onopen = () => {
   let h3_ws_status = document.querySelector('.h3_ws_status')
